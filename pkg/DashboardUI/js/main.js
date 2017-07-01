b6944d060dde44751ad1a768eb570c817a23807e
Ryan Timmons <ryan@rytim.com>
2017-07-01 14:10:08 -0400

    loading

--- pkg/DashboardUI/js/main.js
+++ pkg/DashboardUI/js/main.js
@@ -1,8 +1,11 @@
 $(() => {
   $('td[data-urls]').click(function(e) {
-    var urls = $(this).data('urls').split(/\s+/)
+    var t = $(this);
+    var old = t.clone(true);
+    t.html('🤔')
+    var urls = t.data('urls').split(/\s+/)
     urls.forEach((url) => {
-      $.get(url, {}, (resp) => $(this).html(resp));
+      $.get(url, {}, (resp) => { console.log(url, resp); t.replaceWith(old) });
     });
   });
 });
