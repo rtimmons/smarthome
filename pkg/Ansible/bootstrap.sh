812287509e1f860d3881fd980d7afe58276be3c8
Ryan Timmons <rtimmons@betterment.com>
2017-07-15 14:48:40 -0400

    Download dasher, node-sono-http-api at bootstrap time

--- pkg/Ansible/bootstrap.sh
+++ pkg/Ansible/bootstrap.sh
@@ -5,3 +5,23 @@ if [ ! -e "/Applications/Docker.app" ]; then
 fi
 
 docker build . >/dev/null
+
+if [ ! -e "root" ]; then
+    mkdir root
+fi
+
+pushd root >/dev/null
+    if [ ! -d dasher ]; then
+        git clone https://github.com/maddox/dasher.git
+        pushd dasher >/dev/null
+            npm install
+        popd
+    fi
+
+    if [ ! -d "node-sonos-http-api" ]; then
+        git clone https://github.com/jishi/node-sonos-http-api
+        pushd node-sonos-http-api >/dev/null
+            npm install --production
+        popd >/dev/null
+    fi
+popd >/dev/null
