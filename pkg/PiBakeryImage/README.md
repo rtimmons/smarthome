99978cda348d0cbbbe6e9adeccf06d25cd67da98
Ryan Timmons <ryan@rytim.com>
2017-07-15 11:10:40 -0400

    Cleanup

---
+++ pkg/PiBakeryImage/README.md
@@ -0,0 +1,4 @@
+tasks.TODO
+
+- crib the interesting stuff from davidferguson's cool [pibakery](https://github.com/davidferguson/pibakery)
+
---
+++ pkg/PiBakeryImage/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# download the installer
+INSTALLER="https://github.com/davidferguson/pibakery/releases/download/v0.3.6/PiBakery.pkg"
+
+# lol
+echo -e "Download and install\n$INSTALLER"
