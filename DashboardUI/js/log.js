b3d6686f57b6e4e1067c3f1c83d47ea03087edfe
Ryan Timmons <ryan.timmons@10gen.com>
2018-03-30 13:41:16 -0400

    don't double-log same message

--- DashboardUI/js/log.js
+++ DashboardUI/js/log.js
@@ -4,14 +4,23 @@
     var old = console.log;
     var logger = $('#log');
     logger.hide();
+
+    var lastLog = null;
     console.log = function () {
         var args = Array.prototype.slice.call(arguments);
         old(args);
         args = args.map(x => typeof x == 'object'
             ? JSON.stringify(x) : new String(x)
-        );
+        ).join(' ');
+
+        // don't warn the same thing multiple times
+        if (args == lastLog) {
+            return;
+        }
+        lastLog = args;
+
         logger.stop();
-        logger.html(args.join(' '));
+        logger.html(args);
         logger.fadeIn(20).promise().then(() => logger.fadeOut(1500));
     }
 })();
