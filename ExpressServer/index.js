edbaaa71476d3bdb5ae183b221f87d4afa5e5f9e
Ryan Timmons <ryan.timmons@mongodb.com>
2018-06-20 18:38:30 -0400

    redirs

--- ExpressServer/index.js
+++ ExpressServer/index.js
@@ -1,11 +1,24 @@
-process.title = "smhexprsrv" // name can't be much longer; matches with stop in package.json
+;process.title = "smhexprsrv" // name can't be much longer; matches with stop in package.json
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world');
-})
+var redirs = {
+  '/npr':   'http://smarterhome.local:5005/Bedroom/favorite/Play%20NPR%20One',
+  '/zero':  'http://smarterhome.local:5005/Bedroom/favorite/Zero%207%20Radio',
+  '/up':    'http://smarterhome.local:5005/Bedroom/volume/+1',
+  '/down':  'http://smarterhome.local:5005/Bedroom/volume/-1'
+};
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+for(var path in redirs) {
+  var dest = redirs[path];
+  app.get(path, function(req, res) {
+    res.redirect(301, dest);
+  });
+}
+
+app.get('/', function(req, res){
+  res.send(JSON.stringify(redirs));
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
