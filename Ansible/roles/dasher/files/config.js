f93a8dc3adddde8438acb6e6ac9baf1e6b736f00
Ryan Timmons <ryan.timmons@mongodb.com>
2019-08-24 13:42:08 -0400

    add new dash buttons

--- Ansible/roles/dasher/files/config.js
+++ Ansible/roles/dasher/files/config.js
@@ -13,5 +13,19 @@
     "protocol": "arp",
     "url": "http://localhost:5005/preset/Living-tv",
     "method": "GET"
+  }, {
+    "name": "Bedroom Button 1 (Gillette)",
+    "address": "a0:02:dc:e9:50:e8",
+    "timeout": "120000",
+    "protocol": "arp",
+    "url": "http://localhost:5005/preset/Bedroom-tv",
+    "method": "GET"
+  }, {
+    "name": "Bedroom Button 2 (Doubleshot)",
+    "address": "a0:02:dc:82:60:50",
+    "timeout": "120000",
+    "protocol": "arp",
+    "url": "http://localhost:5005/preset/Bedroom-tv",
+    "method": "GET"
   }]
 }
