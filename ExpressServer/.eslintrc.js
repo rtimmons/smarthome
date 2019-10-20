facb4f6d8c08a1b6af2bc6dbe66109619a093959
Ryan Timmons <ryan.timmons@mongodb.com>
2019-10-20 16:55:12 -0400

    npm run ui-build 'compiles' main.js

--- ExpressServer/.eslintrc.js
+++ ExpressServer/.eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "prettier",
     "rules":{
         "no-console":0
+    },
+    "parserOptions": {
+        "ecmaVersion": 10,
+        "sourceType": "module",
     }
 };
