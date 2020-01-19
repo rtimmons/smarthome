46f663c55f1fa34ed725a5361c48eae0262efae8
Ryan Timmons <ryan.timmons@mongodb.com>
2020-01-19 14:52:54 -0500

    clean in get-hass-configs

--- get-hass-configs.sh
+++ get-hass-configs.sh
@@ -4,3 +4,6 @@ cd "$(dirname "$0")"
 scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.xml' ./HomeAssistantConfig
 scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.yaml' ./HomeAssistantConfig
 scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/.storage' ./HomeAssistantConfig
+
+cd HomeAssistantConfig
+git clean -dfx .
