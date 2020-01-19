727c875778fd6ed1c418997b9004caf921bba2fc
Ryan Timmons <ryan.timmons@mongodb.com>
2020-01-19 13:44:40 -0500

    script to get remote HomeAssistantConfig

---
+++ get-hass-configs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.xml' ./HomeAssistantConfig
+scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.yaml' ./HomeAssistantConfig
+scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/.storage' ./HomeAssistantConfig
