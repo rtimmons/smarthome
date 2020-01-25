f8bfd2b764252b7f866460a4876a54b60660c4e8
Ryan Timmons <ryan.timmons@mongodb.com>
2020-01-25 14:32:20 -0500

    write to yaml

--- MetaHassConfig/hassmetaconfig/cli.py
+++ MetaHassConfig/hassmetaconfig/cli.py
@@ -1,7 +1,16 @@
 import sys
+import hassmetaconfig.gen as gen
+import yaml
 
 
 def main(args=None):
     if args is None:
         args = sys.argv
-    print(f"got args={args}")
+    config = gen.MetaConfig(args[1])
+    automations = gen.Automation(config).gen()
+    scenes = gen.Scenes(config).gen()
+
+    with open('automations.yml', 'w') as f:
+        yaml.dump(automations, f)
+    with open('scenes.yaml', 'w') as f:
+        yaml.dump(scenes, f)
