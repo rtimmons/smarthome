793a76d39e5a599b28b9af2ece77642aae46d2c0
Michael Lee <leemic@amazon.com>
2020-01-29 09:14:21 -0500

    Update python file permissions, update blinds controllers to use smarterhome.local

--- ExpressServer/src/public/js/blind-controller.js
+++ ExpressServer/src/public/js/blind-controller.js
@@ -8,11 +8,8 @@ class BlindController {
 
     move(args) { // ['$room', 'Up/Down/Mid']
         const currRoom = this.app.currentRoom();
-        //const replaceName = n => n.replace(/\$room/g, currRoom);
-        //paths = paths.map(a => replaceName(a));
-        // => ['Living', 'High']
 
-        const path = 'http://raspberrypi.local/BlindControl/cgi-bin/blind-control.py?room=' + currRoom + '&direction=' + args[0];
+        const path = 'http://smarterhome.local/BlindControl/cgi-bin/blind-control.py?room=' + currRoom + '&direction=' + args[0];
         console.log('Requesting to move blinds at url ', path);
         this.app.request(path);
     }
