8630f97cdd8629d7571428140da0d66c830fbc1a
Ryan Timmons <ryan.timmons@mongodb.com>
2020-02-05 10:07:10 -0500

    💅

--- check-hass-configs.sh
+++ check-hass-configs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 cd "$(dirname "$0")" || exit 1
 
 if [ ! -d venv ]; then
@@ -7,10 +9,11 @@ if [ ! -d venv ]; then
     virtualenv venv
 fi
 
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 # shellcheck disable=SC1091
 source ./venv/bin/activate
 
-python3 -m pip install homeassistant
+python3 -m pip install homeassistant  -q -q
 
 hass -c "$PWD/HomeAssistantConfig" --script check_config
 
