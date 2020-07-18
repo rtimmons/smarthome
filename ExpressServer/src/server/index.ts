a53982b67208446a90af0f528020090dc837a06f
Ryan Timmons <ryan.timmons@10gen.com>
2020-07-18 14:01:17 -0400

    try to make stop more robust

--- ExpressServer/src/server/index.ts
+++ ExpressServer/src/server/index.ts
@@ -5,6 +5,9 @@ import * as morgan from 'morgan';
 import * as path from 'path';
 import * as serveFavicon from 'serve-favicon';
 
+// The below value matches with stop in package.json.
+process.title = 'smhexprsrv';
+
 import {blindControli2c} from "./blindControl-i2c";
 import {hass} from './hass';
 import {redirs} from './redirs';
@@ -27,9 +30,6 @@ app.use(redirs);
 app.use(sonos);
 app.use(hass);
 app.use(blindControli2c);
-
-// The below value matches with stop in package.json.
-process.title = 'smhexprsrv';
 
 // Run the thing.
 app.listen(3000, () => console.log('Listening on port 3000!'));
