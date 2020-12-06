57b27f47691561c831e784d6e3bf634e88d1268c
Michael Lee <michaellee1019@gmail.com>
2020-12-06 11:23:59 -0500

    Octoprint server config

---
+++ OctoPrintConfig/printerProfiles/_default.profile
@@ -0,0 +1,33 @@
+axes:
+  e:
+    inverted: false
+    speed: 300
+  x:
+    inverted: false
+    speed: 6000
+  y:
+    inverted: false
+    speed: 6000
+  z:
+    inverted: false
+    speed: 200
+color: default
+extruder:
+  count: 1
+  nozzleDiameter: 0.4
+  offsets:
+  - - 0.0
+    - 0.0
+  sharedNozzle: false
+heatedBed: true
+heatedChamber: false
+id: _default
+model: Prusa i3 MK3S
+name: Prusa
+volume:
+  custom_box: false
+  depth: 210.0
+  formFactor: rectangular
+  height: 210.0
+  origin: lowerleft
+  width: 250.0
--- check-hass-configs.sh
+++ check-hass-configs.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+# set -eo pipefail
 
-cd "$(dirname "$0")" || exit 1
+# cd "$(dirname "$0")" || exit 1
 
-pushd MetaHassConfig >/dev/null 2>&1 || exit 1
-    if [ ! -d venv ]; then
-        python3 -mvenv venv
-    fi
-    export VIRTUAL_ENV_DISABLE_PROMPT=1
-    # shellcheck disable=SC1091
-    source ./venv/bin/activate
-        python3 ./setup.py develop >/dev/null
-        pip install -r ./requirements.txt
-        pushd ../HomeAssistantConfig >/dev/null 2>&1 || exit 1
-            hassmetagen ./metaconfig.yaml
-        popd >/dev/null 2>&1 || exit 1
-    deactivate
-popd >/dev/null 2>&1 || exit 1
+# pushd MetaHassConfig >/dev/null 2>&1 || exit 1
+#     if [ ! -d venv ]; then
+#         python3 -mvenv venv
+#     fi
+#     export VIRTUAL_ENV_DISABLE_PROMPT=1
+#     # shellcheck disable=SC1091
+#     source ./venv/bin/activate
+#         python3 ./setup.py develop >/dev/null
+#         pip install -r ./requirements.txt
+#         pushd ../HomeAssistantConfig >/dev/null 2>&1 || exit 1
+#             hassmetagen ./metaconfig.yaml
+#         popd >/dev/null 2>&1 || exit 1
+#     deactivate
+# popd >/dev/null 2>&1 || exit 1
 
-python3 -m pip install homeassistant  -q -q
+# python3 -m pip install homeassistant  -q -q
 
-hash -r
+# hash -r
 
-hass -c "$PWD/HomeAssistantConfig" --script check_config
+# hass -c "$PWD/HomeAssistantConfig" --script check_config
 
