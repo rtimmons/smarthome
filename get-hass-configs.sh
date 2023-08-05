49707f412d7ee00545a9563d2c35bc40b5f1d655
Ryan Timmons <notreal@notreal.com>
2023-08-05 10:59:05 -0400

    hold on to your butts

--- get-hass-configs.sh
+++ get-hass-configs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.xml' ./HomeAssistantConfig
-scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/*.yaml' ./HomeAssistantConfig
-scp -r 'pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/.storage' ./HomeAssistantConfig
+scp -r 'udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig/*.xml' ./HomeAssistantConfig
+scp -r 'udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig/*.yaml' ./HomeAssistantConfig
+scp -r 'udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig/.storage' ./HomeAssistantConfig
 
 cd HomeAssistantConfig
 git clean -dfx .
--- put-hass-configs.sh
+++ put-hass-configs.sh
@@ -6,25 +6,25 @@ cd "$(dirname "$0")" || exit 1
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-if ! ssh -o PasswordAuthentication=no -o BatchMode=yes pi@smarterhome.local exit &>/dev/null; then
-    echo -e "Need passwordless ssh:\n\n    ssh-copy-id pi@smarterhome.local\n"
+if ! ssh -o PasswordAuthentication=no -o BatchMode=yes udoobolt@smarterudoo.local exit &>/dev/null; then
+    echo -e "Need passwordless ssh:\n\n    ssh-copy-id udoobolt@smarterudoo.local\n"
     exit 1
 fi
 
 ./check-hass-configs.sh
 
 echo "Stopping home-assistant"
-ssh -o PasswordAuthentication=no -o BatchMode=yes pi@smarterhome.local \
+ssh -o PasswordAuthentication=no -o BatchMode=yes udoobolt@smarterudoo.local \
     'sudo systemctl stop home-assistant.service'
 
 pushd HomeAssistantConfig >/dev/null 2>&1 || exit 1
-    scp ./*.yaml pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/ >/dev/null
-    scp ./*.xml pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig/ >/dev/null
-    # scp -r .storage pi@smarterhome.local:/home/pi/repo/HomeAssistantConfig >/dev/null
+    scp ./*.yaml udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig/ >/dev/null
+    scp ./*.xml  udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig/ >/dev/null
+    # scp -r .storage udoobolt@smarterudoo.local:/home/udoobolt/repo/HomeAssistantConfig >/dev/null
 popd >/dev/null 2>&1 || exit 1
 
 echo "Restarting home-assistant for new changes to take effect"
-ssh -o PasswordAuthentication=no -o BatchMode=yes pi@smarterhome.local \
+ssh -o PasswordAuthentication=no -o BatchMode=yes udoobolt@smarterudoo.local \
     'sudo systemctl restart home-assistant.service'
 
-echo -e "Home-assistant is now restarting. Check Z-Wave at\n    http://smarterhome.local:8123/lovelace\n"
+echo -e "Home-assistant is now restarting. Check Z-Wave at\n    http://smarterudoo.local:8123/lovelace\n"
