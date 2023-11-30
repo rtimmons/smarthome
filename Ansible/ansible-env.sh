dc6df7087c57991c4279a5c819e3469affce0a35
Ryan Timmons <ryan.timmons@mongodb.com>
2023-11-30 10:51:13 -0500

    wip on making crypto work for apple silicon

--- Ansible/ansible-env.sh
+++ Ansible/ansible-env.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "venv" || ! -e "venv/setup2" ]]; then
+if [[ ! -d "venv" || ! -e "venv/setup3" ]]; then
+
+    declare -a crypto_cmd
+    crypto_cmd=()
+    if [[ "$(uname)" == "Darwin" ]]; then
+        brew install "openssl@3"
+        crypto_cmd=(
+            env OPENSSL_DIR="$(brew --prefix openssl@3)"
+        )
+    fi
+
+    crypto_cmd=("${crypto_cmd[@]}" pip install cryptography)
+
     rm -rf "venv"
     python3 -mvenv venv
     # shellcheck source=/dev/null
     set +u
+    set -x
         VIRTUAL_ENV_DISABLE_PROMPT=true source ./venv/bin/activate
             python3 -m pip install --upgrade pip
+            echo "${crypto_cmd[@]}"
+            "${crypto_cmd[@]}"
             python3 -m pip install ansible
             python3 -m pip install yamllint
-            touch venv/setup2
+            touch venv/setup3
         deactivate
+    set +x
     set -u
 fi
 
--- check-hass-configs.sh
+++ check-hass-configs.sh
@@ -6,16 +6,33 @@ cd "$(dirname "$0")" || exit 1
 
 # Create venv for MetaHassConfig
 pushd MetaHassConfig >/dev/null 2>&1 || exit 1
-    if [[ ! -d "venv" || ! -e "venv/setup3" ]]; then
+    if [[ ! -d "venv" || ! -e "venv/setup4" ]]; then
+
+        declare -a ssl_prefix
+        ssl_prefix=()
+        if [[ "$(uname)" == "Darwin" ]]; then
+            brew install "openssl@3"
+            ssl_prefix=(
+                env OPENSSL_DIR="$(brew --prefix openssl@3)"
+            )
+            OPENSSL_DIR="$(brew --prefix openssl@3)"
+            export OPENSSL_DIR
+        fi
+        hass_cmd=(  "${ssl_prefix[@]}" python3 -m pip install 'homeassistant==2021.9.7'  -q -q)
+        crypto_cmd=("${ssl_prefix[@]}" python3 -m pip install "cryptography==3.3.2")
+
         rm -rf "venv"
         python3 -mvenv venv
         # shellcheck source=/dev/null
         VIRTUAL_ENV_DISABLE_PROMPT=true source ./venv/bin/activate
             python3 -m pip install --upgrade pip
+            echo "${crypto_cmd[@]}"
+            "${crypto_cmd[@]}"
             python3 ./setup.py develop >/dev/null
             python3 -m pip install -r ./requirements.txt
-            python3 -m pip install 'homeassistant==2021.9.7'  -q -q
-            touch venv/setup3
+            echo "${hass_cmd[@]}"
+            "${hass_cmd[@]}"
+            touch venv/setup4
         deactivate
     fi
 popd >/dev/null 2>&1 || exit 1
