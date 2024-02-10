1d6cbd52a5be4e69be826d5753a53b5649cabe89
Ryan Timmons <rtimmons@RyWasMongos-2.local>
2024-02-09 19:01:17 -0500

    don't validate the generated config, just generate it

--- check-hass-configs.sh
+++ check-hass-configs.sh
@@ -4,6 +4,19 @@ set -eo pipefail
 
 cd "$(dirname "$0")" || exit 1
 
+# TODO: This script _used_ to both generate the 
+#       full config and then valiate that the
+#       fully generated hass config was valid.
+#       But it's hard to keep this old hass
+#       installation working on modern laptops.
+#       So the lines that did that are commented
+#       out by the same commit that introduced
+#       this text. Either fix the verification or
+#       rename this to reflect its new purpose.
+#
+#       Now this just generates the full config.
+
+
 # Create venv for MetaHassConfig
 pushd MetaHassConfig >/dev/null 2>&1 || exit 1
     if [[ ! -d "venv" || ! -e "venv/setup4" ]]; then
@@ -18,8 +31,8 @@ pushd MetaHassConfig >/dev/null 2>&1 || exit 1
             OPENSSL_DIR="$(brew --prefix openssl@3)"
             export OPENSSL_DIR
         fi
-        hass_cmd=(  "${ssl_prefix[@]}" python3 -m pip install 'homeassistant==2021.9.7'  -q -q)
-        crypto_cmd=("${ssl_prefix[@]}" python3 -m pip install "cryptography==3.3.2")
+        # hass_cmd=(  "${ssl_prefix[@]}" python3 -m pip install 'homeassistant==2021.9.7'  -q -q)
+        # crypto_cmd=("${ssl_prefix[@]}" python3 -m pip install "cryptography==3.3.2")
 
         rm -rf "venv"
         python3 -mvenv venv
@@ -28,12 +41,12 @@ pushd MetaHassConfig >/dev/null 2>&1 || exit 1
             export PIP_CONSTRAINT="$PWD/venv/constraints.txt"
             echo "cython < 3.0" > "${PIP_CONSTRAINT}"
             python3 -m pip install --upgrade pip
-            echo "${crypto_cmd[@]}"
-            "${crypto_cmd[@]}"
+            # echo "${crypto_cmd[@]}"
+            # "${crypto_cmd[@]}"
             python3 ./setup.py develop >/dev/null
             python3 -m pip install -r ./requirements.txt
-            echo "${hass_cmd[@]}"
-            "${hass_cmd[@]}"
+            # echo "${hass_cmd[@]}"
+            # "${hass_cmd[@]}"
             touch venv/setup4
         deactivate
     fi
@@ -45,8 +58,8 @@ pushd HomeAssistantConfig >/dev/null 2>&1 || exit 1
     VIRTUAL_ENV_DISABLE_PROMPT=true source ../MetaHassConfig/venv/bin/activate
         hash -r
         hassmetagen ./metaconfig.yaml
-        hass -c "$PWD" --script check_config
+        # hass -c "$PWD" --script check_config
     deactivate
 popd >/dev/null 2>&1 || exit 1
 
-echo "Checked home-assistant config at $PWD/HomeAssistantConfig."
+echo "Generated home-assistant config at $PWD/HomeAssistantConfig."
