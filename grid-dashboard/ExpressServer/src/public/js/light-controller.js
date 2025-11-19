492691c4b956206c448459da6a570681b968046f
Ryan Timmons <ryan@rytim.com>
2025-11-18 20:06:08 -0500

    fix light controller pathing

--- grid-dashboard/ExpressServer/src/public/js/light-controller.js
+++ grid-dashboard/ExpressServer/src/public/js/light-controller.js
@@ -10,10 +10,21 @@ class LightController {
         const currRoom = this.app.currentRoom();
         const replaceName = n => n.replace(/\$room/g, currRoom);
         paths = paths.map(a => replaceName(a));
-        // => ['Living', 'High']
+        // => ['Living Room', 'High']
 
-        const path = '/scenes/scene_' + paths.join('_').replace(/\s+/g, '_').toLowerCase();
-        console.log('Requesting scene at url ', path);
-        this.app.request(path);
+        const scenePath = 'scenes/scene_' + paths.join('_').replace(/\s+/g, '_').toLowerCase();
+
+        // Build URL using current page's base path for ingress compatibility
+        let url;
+        if (!this.root) {
+            // Get base path from current location, removing ALL trailing slashes
+            const basePath = window.location.pathname.replace(/\/+$/, ''); // Remove all trailing slashes
+            url = basePath + '/' + scenePath;
+        } else {
+            url = this.root + '/' + scenePath;
+        }
+
+        console.log('Requesting scene at url ', url);
+        this.app.request(url);
     }
 }
